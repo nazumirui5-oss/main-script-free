37df5f7ba73ad3f871d03aac414bda6a73d08b52
nazumirui5-oss <nazumirui5@gmail.com>
2026-06-10 09:39:20 +0700

    Update mainscriptfree.lua

--- mainscriptfree.lua
+++ mainscriptfree.lua
@@ -1,1 +1,15 @@
 loadstring(game:HttpGet("https://luapot.com/api/loadstring/b2e32af3fe21d5d9d16302c791682713"))()
+
+--- gng what are u even doin here 😭
+
+--- came lookin for the original source?
+
+--- ts got me cryin 😭🙏
+
+--- u searched all this way just to find absolutely nothin
+
+--- the real source not here lil bro
+
+--- close the tab and call it a day 💔
+
+--- tuff.
